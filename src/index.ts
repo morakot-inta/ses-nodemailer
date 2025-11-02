924b75f007225e0bbf4f5a2c003aabeddd2abcf2
morakot <morakot.inta@gmail.com>
2025-11-02 20:36:48 +0700

    feat: change email sender

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { sendEmail } from './service/sendmail.js';
 
-sendEmail('morakot.i@kaopanwa.co.th', 'Test Subject', 'Test email body');
+sendEmail('test@example.com', 'Test Subject', 'Test email body');
